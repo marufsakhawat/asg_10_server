2734789f701b08b75e27d7e8d0b398ed00600588
Maruf Sakhawat <marufsakhawat@gmail.com>
2025-12-07 03:13:31 +0600

    get and post function added

--- index.js
+++ index.js
@@ -32,6 +32,54 @@ async function run() {
     // await client.close();  // default but I make it commented
   }
 }
+
+ // HomePage er Jonno 6 Listing limit Set Kora
+  app.get("/listings-recent", async (req, res) => {
+    const result = await listingsCollection
+      .find()
+      .sort({ _id: -1 })
+      .limit(6)
+      .toArray();
+    res.send(result);
+  });
+
+  // Notun listing add to DB
+
+  app.post("/listings", async (req, res) => {
+    const newListing = req.body;
+    const result = await listingsCollection.insertOne(newListing);
+    res.send(result);
+  });
+
+  //  ALL listings Pets & Supplies page e add kora
+
+  app.get("/all-listings", async (req, res) => {
+    const result = await listingsCollection.find().toArray();
+    res.send(result);
+  });
+
+  // Get single listing by ID details page Er Jonno
+
+  app.get("/listing/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await listingsCollection.findOne(query);
+    res.send(result);
+  });
+
+  // Database e Order Save Kora
+
+  app.post("/orders", async (req, res) => {
+    const newOrder = req.body;
+    const result = await ordersCollection.insertOne(newOrder);
+    res.send(result);
+  });
+
+
+
+
+
+
 run().catch(console.dir);
 
 app.get('/', (req, res)=>{
